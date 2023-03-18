47d4f12365fd89d3a2c2193362907ad53003d94a
endlmk <1269467+endlmk@users.noreply.github.com>
2023-03-18 23:55:40 +0900

    Change u8 to ArrayList(u8).

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,20 @@
 const std = @import("std");
 const testing = std.testing;
 
-export fn parseVariant(b :u8) i32 {
-    return b;
+export fn parseVariant(b :*std.ArrayList(u8)) i32 {
+    return b.items[0];
 }
 
 test "parseVariant" {
-    try testing.expect(parseVariant(0b0000001) == 1);
+    var list = std.ArrayList(u8).init(std.testing.allocator);
+    try list.append(0b00000001);
+
+    try testing.expect(parseVariant(&list) == 1);
+
+    list.clearRetainingCapacity();
+    try list.append(0b01111111);
+    try testing.expect(parseVariant(&list) == 127);
+    list.deinit();
+    // try testing.expect(parseVariant(0b1000000000000001) == 128);
+
 }
