61948fbb95e898ab5e85c4ea8fca7bfecd6ecd56
endlmk <1269467+endlmk@users.noreply.github.com>
2023-03-19 09:13:14 +0900

    Fix argument type.

--- src/main.zig
+++ src/main.zig
@@ -1,19 +1,19 @@
 const std = @import("std");
 const testing = std.testing;
 
-export fn parseVariant(b :*std.ArrayList(u8)) i32 {
-    return b.items[0];
+fn parseVariant(b :[]const u8) i32 {
+    return b[0];
 }
 
 test "parseVariant" {
     var list = std.ArrayList(u8).init(std.testing.allocator);
     try list.append(0b00000001);
 
-    try testing.expect(parseVariant(&list) == 1);
+    try testing.expect(parseVariant(list.items) == 1);
 
     list.clearRetainingCapacity();
     try list.append(0b01111111);
-    try testing.expect(parseVariant(&list) == 127);
+    try testing.expect(parseVariant(list.items) == 127);
     list.deinit();
     // try testing.expect(parseVariant(0b1000000000000001) == 128);
 
